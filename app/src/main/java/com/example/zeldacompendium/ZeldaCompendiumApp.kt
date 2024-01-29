cdfc7f7b3fcbb43f2812335b3c0ff661c2fd3aba
Javier Martin Oliveros <javier.martinoliveros@gruposantander.com>
2024-01-29 08:02:59 +0100

    Image catching

--- app/src/main/java/com/example/zeldacompendium/ZeldaCompendiumApp.kt
+++ app/src/main/java/com/example/zeldacompendium/ZeldaCompendiumApp.kt
@@ -1,11 +1,32 @@
 package com.example.zeldacompendium
 
 import android.app.Application
+import android.os.strictmode.DiskReadViolation
+import coil.ImageLoader
+import coil.ImageLoaderFactory
+import coil.disk.DiskCache
+import coil.memory.MemoryCache
+import coil.request.CachePolicy
+import coil.util.DebugLogger
 import dagger.hilt.android.HiltAndroidApp
 import timber.log.Timber
 
 @HiltAndroidApp
-class ZeldaCompendiumApp : Application() {
+class ZeldaCompendiumApp : Application(), ImageLoaderFactory {
+    override fun newImageLoader(): ImageLoader {
+        return ImageLoader(this)
+            .newBuilder()
+            .diskCachePolicy(CachePolicy.ENABLED)
+            .diskCache {
+                DiskCache.Builder()
+                    .directory(cacheDir)
+                    .maxSizePercent(0.04)
+                    .build()
+            }
+            .respectCacheHeaders(false)
+            .logger(DebugLogger())
+            .build()
+    }
     override fun onCreate() {
         super.onCreate()
         Timber.plant(Timber.DebugTree())
