f2cb49e3fdb5dedc37775f00d5cf2640dcb30be5
jmrtno <javiermartino96@gmail.com>
2024-02-02 01:45:49 +0100

    Estilo botones pantalla de inicio

---
+++ app/src/main/java/com/example/zeldacompendium/ui/CustomButtom.composable.kt
@@ -0,0 +1,86 @@
+package com.example.zeldacompendium.ui
+
+import androidx.annotation.DrawableRes
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.RowScope
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Shape
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.zeldacompendium.R
+
+@Composable
+fun ImageButton(
+   onClick: () -> Unit,
+   @DrawableRes backgroundDrawableId: Int,
+   modifier: Modifier = Modifier,
+   shape: Shape = RoundedCornerShape(5.dp),
+   border: BorderStroke? = null,
+   contentPadding: PaddingValues = ButtonDefaults.ContentPadding,
+   content: @Composable RowScope.() -> Unit
+) {
+   Button(
+      modifier = modifier,
+      onClick = onClick,
+      contentPadding = PaddingValues(0.dp),
+      shape = shape,
+      border = border,
+   ) {
+      Box(
+         contentAlignment = Alignment.Center,
+      ) {
+         Image(
+            modifier = Modifier.matchParentSize(),
+            painter = painterResource(backgroundDrawableId),
+            contentDescription = "hola",
+            contentScale = ContentScale.FillBounds,
+         )
+         Row(
+            horizontalArrangement = Arrangement.Center,
+            verticalAlignment = Alignment.CenterVertically,
+            modifier = Modifier.padding(contentPadding),
+            content = content,
+         )
+      }
+   }
+}
+@Preview
+@Composable
+fun ImageButtonPreview(
+   contentPadding: PaddingValues = ButtonDefaults.ContentPadding,
+) {
+   ImageButton(
+      onClick = { /* Acción al hacer clic */ },
+      backgroundDrawableId = R.drawable.button_bg,
+      modifier = Modifier
+         .padding(16.dp),
+      shape = RoundedCornerShape(5.dp),
+      border = BorderStroke(2.dp, Color(0xFF946D48)),
+      contentPadding = contentPadding,
+   ) {
+      Text(text = "TOTK", color = Color(0xFFBB8F0B))
+   }
+}
--- app/src/main/java/com/example/zeldacompendium/ui/selectgame/SelectGameContainer.kt
+++ app/src/main/java/com/example/zeldacompendium/ui/selectgame/SelectGameContainer.kt
@@ -1,11 +1,12 @@
 package com.example.zeldacompendium.ui.selectgame
 
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
@@ -13,13 +14,15 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import com.example.zeldacompendium.R
 import androidx.navigation.NavController
 import androidx.navigation.compose.rememberNavController
+import com.example.zeldacompendium.R
+import com.example.zeldacompendium.ui.ImageButton
 
 @Composable
 fun SelectGameContainer(
@@ -44,19 +47,28 @@ fun CompendiumNavigation(
       contentAlignment = Alignment.Center
    ) {
       Column(verticalArrangement = Arrangement.spacedBy(20.dp)) {
-         Button(onClick = {
-            navController.navigate(
-               "compendium_breath_screen"
-            )
-         }) {
-            Text(text = "BOTW")
+         ImageButton(
+            backgroundDrawableId = R.drawable.button_bg,
+            border = BorderStroke(2.dp, Color(0xFF946D48)),
+            onClick = {
+               navController.navigate(
+                  "compendium_breath_screen"
+               )
+            }
+         ) {
+            Text(text = "BOTW", color = Color(0xFFBB8F0B))
          }
-         Button(onClick = {
-            navController.navigate(
-               "compendium_tears_screen"
-            )
-         }) {
-            Text(text = "TOTK")
+
+         ImageButton(
+            backgroundDrawableId = R.drawable.button_bg,
+            border = BorderStroke(2.dp, Color(0xFF946D48)),
+            onClick = {
+               navController.navigate(
+                  "compendium_tears_screen"
+               )
+            }
+         ) {
+            Text(text = "TOTK", color = Color(0xFFBB8F0B))
          }
       }
    }
@@ -67,7 +79,7 @@ fun SetBackgroundImage() {
    Box(modifier = Modifier.fillMaxSize()) {
       Image(
          modifier = Modifier.matchParentSize(),
-         painter = painterResource(id = R.drawable.fondo_compendiumapp),
+         painter = painterResource(id = R.drawable.mainscreen_bg),
          contentDescription = "background",
          contentScale = ContentScale.FillBounds
       )
