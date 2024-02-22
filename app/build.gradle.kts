4a9ed87ff64ff345d9392f0d7a8af4bb05cb29a4
[jmrtno] <[javiermartino96@gmail.com]>
2024-02-22 19:52:03 +0100

    kapt to ksp migration

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,54 +1,55 @@
 plugins {
-    id("com.android.application")
-    id("org.jetbrains.kotlin.android")
-    id("dagger.hilt.android.plugin")
-    id("kotlin-kapt")
+   id("com.android.application")
+   id("org.jetbrains.kotlin.android")
+   id("dagger.hilt.android.plugin")
+   id("com.google.devtools.ksp")
+   id("kotlin-kapt")
 }
 
 android {
-    namespace = "com.example.zeldacompendium"
-    compileSdk = 34
+   namespace = "com.example.zeldacompendium"
+   compileSdk = 34
 
-    defaultConfig {
-        applicationId = "com.example.zeldacompendium"
-        minSdk = 31
-        targetSdk = 34
-        versionCode = 1
-        versionName = "1.0"
+   defaultConfig {
+      applicationId = "com.example.zeldacompendium"
+      minSdk = 31
+      targetSdk = 34
+      versionCode = 1
+      versionName = "1.0"
 
-        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
-        vectorDrawables {
-            useSupportLibrary = true
-        }
-    }
+      testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+      vectorDrawables {
+         useSupportLibrary = true
+      }
+   }
 
-    buildTypes {
-        release {
-            isMinifyEnabled = false
-            proguardFiles(
-                getDefaultProguardFile("proguard-android-optimize.txt"),
-                "proguard-rules.pro"
-            )
-        }
-    }
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
-    }
-    kotlinOptions {
-        jvmTarget = "1.8"
-    }
-    buildFeatures {
-        compose = true
-    }
-    composeOptions {
-        kotlinCompilerExtensionVersion = "1.5.1"
-    }
-    packaging {
-        resources {
-            excludes += "/META-INF/{AL2.0,LGPL2.1}"
-        }
-    }
+   buildTypes {
+      release {
+         isMinifyEnabled = false
+         proguardFiles(
+            getDefaultProguardFile("proguard-android-optimize.txt"),
+            "proguard-rules.pro"
+         )
+      }
+   }
+   compileOptions {
+      sourceCompatibility = JavaVersion.VERSION_1_8
+      targetCompatibility = JavaVersion.VERSION_1_8
+   }
+   kotlinOptions {
+      jvmTarget = "1.8"
+   }
+   buildFeatures {
+      compose = true
+   }
+   composeOptions {
+      kotlinCompilerExtensionVersion = "1.5.1"
+   }
+   packaging {
+      resources {
+         excludes += "/META-INF/{AL2.0,LGPL2.1}"
+      }
+   }
 }
 
 dependencies {
@@ -66,22 +67,22 @@ dependencies {
 
    implementation("com.google.dagger:hilt-android:2.50")
    kapt("com.google.dagger:hilt-android-compiler:2.48")
-   implementation("androidx.hilt:hilt-navigation-compose:1.1.0")
+   implementation("androidx.hilt:hilt-navigation-compose:1.2.0")
    implementation("androidx.constraintlayout:constraintlayout-compose:1.0.1")
 
-   kapt("androidx.room:room-compiler:2.6.1")
+   ksp("androidx.room:room-compiler:2.6.1")
 
-   implementation ("com.jakewharton.timber:timber:5.0.1")
+   implementation("com.jakewharton.timber:timber:5.0.1")
    implementation("androidx.navigation:navigation-compose:2.7.7")
 
-   implementation ("com.squareup.retrofit2:retrofit:2.9.0")
-   implementation ("com.squareup.retrofit2:converter-gson:2.9.0")
+   implementation("com.squareup.retrofit2:retrofit:2.9.0")
+   implementation("com.squareup.retrofit2:converter-gson:2.9.0")
 
-   implementation ("com.squareup.okhttp3:okhttp:4.12.0")
-   implementation ("com.squareup.okhttp3:logging-interceptor:4.12.0")
+   implementation("com.squareup.okhttp3:okhttp:4.12.0")
+   implementation("com.squareup.okhttp3:logging-interceptor:4.12.0")
    implementation("androidx.palette:palette-ktx:1.0.0")
    implementation("io.coil-kt:coil-compose:2.5.0")
-   implementation ("com.google.accompanist:accompanist-coil:0.7.0")
+   implementation("com.google.accompanist:accompanist-coil:0.7.0")
 
 
    testImplementation("junit:junit:4.13.2")
