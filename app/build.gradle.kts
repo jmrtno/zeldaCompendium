db08f271235a621f2572139f6235e17470fff687
[jmrtno] <[javiermartino96@gmail.com]>
2024-05-16 00:44:57 +0200

    update implementations

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -54,16 +54,15 @@ android {
 
 dependencies {
 
-   implementation("androidx.core:core-ktx:1.12.0")
-   implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.7.0")
-   implementation("androidx.activity:activity-compose:1.8.2")
+   implementation("androidx.core:core-ktx:1.13.1")
+   implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.8.0")
+   implementation("androidx.activity:activity-compose:1.9.0")
    implementation(platform("androidx.compose:compose-bom:2023.08.00"))
 
    implementation("androidx.compose.ui:ui")
    implementation("androidx.compose.ui:ui-graphics")
    implementation("androidx.compose.ui:ui-tooling-preview")
-
-   implementation("androidx.compose.material3:material3:1.2.0")
+   implementation("androidx.compose.material3:material3:1.2.1")
 
    implementation("com.google.dagger:hilt-android:2.50")
    kapt("com.google.dagger:hilt-android-compiler:2.48")
