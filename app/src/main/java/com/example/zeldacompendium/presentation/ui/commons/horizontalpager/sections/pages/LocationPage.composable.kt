43028dace5be7d263516e5584d44db748b390906
Javier Martin Oliveros <javier.martinoliveros@gruposantander.com>
2024-07-17 12:53:46 +0200

    Location row now navigates. Underline text

--- app/src/main/java/com/example/zeldacompendium/presentation/ui/commons/horizontalpager/sections/pages/LocationPage.composable.kt
+++ app/src/main/java/com/example/zeldacompendium/presentation/ui/commons/horizontalpager/sections/pages/LocationPage.composable.kt
@@ -1,9 +1,12 @@
 package com.example.zeldacompendium.presentation.ui.commons.horizontalpager.sections.pages
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -11,6 +14,9 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.style.TextDecoration
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.zeldacompendium.R
 import com.example.zeldacompendium.data.remote.responses.ItemDetailModel
@@ -29,27 +35,27 @@ fun LocationPage(
          items(locationCount.size) {
             val commonLocation = itemInfo.data.commonLocations[it]
             Row(
-               modifier = Modifier.fillMaxWidth(),
+               modifier = Modifier
+                  .fillMaxWidth()
+                  .padding(bottom = 5.dp)
+                  .clickable {
+                     ServiceProvider
+                     .navigationService
+                     .navigateTo("locations_map_screen/$gameId/$commonLocation") },
                horizontalArrangement = Arrangement.SpaceBetween,
                verticalAlignment = Alignment.CenterVertically
             ) {
                Text(
                   text = commonLocation,
                   fontSize = 14.sp,
+                  textDecoration = TextDecoration.Underline,
                   color = Color.White
                )
-               IconButton(onClick = {
-                  ServiceProvider
-                  .navigationService
-                  .navigateTo("locations_map_screen/$gameId/$commonLocation") }
-               ) {
-                  Image(
-                     painter = painterResource(R.drawable.map_icon),
-                     contentDescription = "Map icon"
-                  )
-               }
+               Image(
+                  painter = painterResource(R.drawable.map_icon),
+                  contentDescription = "Map icon"
+               )
             }
-
          }
       } else {
          item {
